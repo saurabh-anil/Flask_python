2bf6170230eabee1a270dc532ae64ac989b70396
saurabh-anil <saurabh.anil@netomi.com>
2025-03-19 22:43:58 +0530

    updated code

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
+@app.route('/')
+def home():
     return "<p>Hello, World! Vaibhav Python</p>"
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username == 'admin' and password == 'password':
+            return redirect(url_for('home'))
+        else:
+            return render_template('login.html', error='Invalid Credentials')
+    return render_template('login.html', error=None)
+
+if __name__ == '__main__':
+    app.run(debug=True)
